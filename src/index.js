7d23915bb8c5eeaab44cbbd558a017c88b457c39
CY Kalu <cy23@tutanota.com>
2021-08-18 17:46:55 +0000

    Convert styles to SASS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/index.css';
+import './styles/index.scss';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
