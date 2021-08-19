0489db57e2867f5ec2c42ca8b08a1e4186608e4c
CY Kalu <cy23@tutanota.com>
2021-08-19 13:53:15 +0000

    Make navbar sticky

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 
 const Header = () => (
-  <Navbar bg="light" expand="lg">
+  <Navbar className="sticky-top" bg="light" expand="lg">
     <Container>
       <Navbar.Brand href="#home" className="fw-bold">Movie Face</Navbar.Brand>
     </Container>
