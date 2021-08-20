853e0bd161d90a37bf7217db2d0c8eace8c0da88
CY Kalu <cy23@tutanota.com>
2021-08-20 20:57:21 +0000

    Add linebreaks in review comment

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -18,7 +18,8 @@ const MovieReviews = (props) => {
           <li className="list-group-item px-0 bg-transparent text-white" key={review.id}>
             <h6 className="text-info mb-0 lh-1">{`${review.author}`}</h6>
             <small className="text-muted">{`Rating: ${review.author_details.rating}`}</small>
-            <p className="lh-1 opacity-75">{review.content}</p>
+
+            {review.content.split('\n').map((line) => (<p className="mb-2 opacity-75" key={Math.random()}>{line}</p>))}
           </li>
         ))}
       </ul>
