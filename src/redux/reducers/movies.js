c18df923a6040229a1678421724969591ec165f6
CY Kalu <cy23@tutanota.com>
2021-08-20 03:40:14 +0000

    Remove movies in previous state

--- src/redux/reducers/movies.js
+++ src/redux/reducers/movies.js
@@ -3,7 +3,7 @@ import { ADD_MOVIES } from '../actions/types';
 export default (state = [], action) => {
   switch (action.type) {
     case ADD_MOVIES:
-      return [...state, ...action.payload];
+      return action.payload;
     default:
       return state;
   }
