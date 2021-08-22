690ef04e63ddb6401bd8d6152521789d22771864
CY Kalu <cy23@tutanota.com>
2021-08-22 12:58:53 +0000

    Swap back drop and poster images

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -31,15 +31,10 @@ const MovieDetail = () => {
   return (
     <Container className="mt-5 text-white mb-5">
       <Row>
-        <Col md={6}>
-          <img className="img-fluid" src={`https://image.tmdb.org/t/p/w780/${movie.backdrop_path}`} alt={movie.title} />
-          {movie.tagline ? <div className="alert alert-info rounded-0 py-2 mb-0">{movie.tagline}</div> : ''}
-
-          <Row className="mt-2">
-            {makeAttrColumn('col-12', 'GENRES', movie.genres.map((genre) => makeBadge(genre.name)))}
-          </Row>
+        <Col md={5} lg={4}>
+          <img className="img-fluid" id="poster" src={`https://image.tmdb.org/t/p/w780/${movie.poster_path}`} alt={movie.title} />
         </Col>
-        <Col md={6}>
+        <Col>
           <h1 className="text-secondarys lh-1 mb-0"><b>{movie.title}</b></h1>
           <a href={movie.homepage} className="text-secondary fw-light" target="_blank" rel="noreferrer">
             <small>{movie.homepage}</small>
@@ -47,23 +42,29 @@ const MovieDetail = () => {
 
           <p className="lh-sm text-info mt-4">{movie.overview}</p>
           <Row>
-            {makeAttrColumn('col-6', 'STATUS', movie.status)}
-            {makeAttrColumn('col-6', 'RELEASE DATE', movie.release_date)}
-            {makeAttrColumn('col-6', 'RUNTIME', `${movie.runtime} Minutes`)}
-            {makeAttrColumn('col-6', 'RATING', `${movie.vote_average} / 10`)}
-            {makeAttrColumn('col-12', 'SPOKEN LANGUAGES', movie.spoken_languages.map((lang) => makeBadge(lang.name)))}
+            {makeAttrColumn('col-6 col-lg-4', 'STATUS', movie.status)}
+            {makeAttrColumn('col-6 col-lg-4', 'RELEASE DATE', movie.release_date)}
+            {makeAttrColumn('col-6 col-lg-4', 'RUNTIME', `${movie.runtime} Minutes`)}
+            {makeAttrColumn('col-6 col-lg-4', 'RATING', `${movie.vote_average} / 10`)}
+            {makeAttrColumn('col-6 col-lg-4', 'VOTE COUNT', `${movie.vote_count}`)}
+            {makeAttrColumn('col-6 col-lg-4', 'POPULARITY', `${movie.popularity}%`)}
+            <Row>
+              {makeAttrColumn('col-md-6', 'GENRES', movie.genres.map((genre) => makeBadge(genre.name)))}
+              {makeAttrColumn('col-md-6', 'SPOKEN LANGUAGES', movie.spoken_languages.map((lang) => makeBadge(lang.name)))}
+            </Row>
           </Row>
         </Col>
       </Row>
 
       <hr className="mb-5" />
 
-      <Row className="justify-content-center overflow-hidden" id="review-row">
+      <Row className="align-items-center row-type-1 overflow-hidden">
         <Col className="overflow-hidden overflow-y-auto h-100 mt-3 mt-md-0 pb-3">
           <MovieReviews movieID={movie.id} />
         </Col>
-        <Col md={5} xl={4} className="order-first order-md-last text-center">
-          <img className="img-fluid" id="poster" src={`https://image.tmdb.org/t/p/w780/${movie.poster_path}`} alt={movie.title} />
+        <Col md={5} className="order-first order-md-last text-center">
+          {movie.tagline ? <div className="alert alert-info border-0 border-bottom border-2 border-info rounded-0 py-2 mb-0 lh-1">{movie.tagline}</div> : ''}
+          <img className="img-fluid" src={`https://image.tmdb.org/t/p/w780/${movie.backdrop_path}`} alt={movie.title} />
         </Col>
       </Row>
     </Container>
