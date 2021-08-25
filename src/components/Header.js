5faa546f45d26e816e26a1fe1aca374b15d2b2a4
CY Kalu <cy23@tutanota.com>
2021-08-25 17:46:02 +0000

    Prefer anchor element: to force reload

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,16 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
-import { Link } from 'react-router-dom';
 import MovieFilter from './MovieFilter';
 import SearchForm from './SearchForm';
 
 const Header = () => (
   <Navbar variant="dark" className="sticky-top text-white" expand="md" style={{ backgroundColor: '#1f1f1f' }}>
     <Container>
-      <Link to="/" className="navbar-brand text-white">
+      <a href="/" className="navbar-brand text-white">
         Movie
         <b className="fw-bold">Face</b>
-      </Link>
+      </a>
       <Navbar.Toggle className="border-0" aria-controls="navbarScroll" />
       <Navbar.Collapse>
         <Nav className="align-items-md-center my-5 my-md-0 ms-auto">
