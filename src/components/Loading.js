432e5bd439f78fbb57afde590d21b317d30ef592
CY Kalu <cy23@tutanota.com>
2021-08-26 11:57:35 +0000

    Make Loading component accept variant prop

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,28 @@
-const Loading = () => (
-  <div className="d-flex flex-column justify-content-center align-items-center mt-5">
-    <div className="spinner-grow bg-light" role="status">
-      <span className="visually-hidden sr-only">Loading...</span>
+import PropTypes from 'prop-types';
+
+const Loading = (props) => {
+  const { variant } = props;
+  let spinnerClass = 'spinner-border';
+
+  if (variant.length > 0) {
+    spinnerClass += ` spinner-border-${variant}`;
+  }
+
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center mt-5" data-testid="loading">
+      <div className={spinnerClass} role="status">
+        <span className="visually-hidden sr-only">Loading...</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
+
+Loading.propTypes = {
+  variant: PropTypes.string,
+};
+
+Loading.defaultProps = {
+  variant: '',
+};
 
 export default Loading;
