652dfc325b5f6b52d0df0d1c597761a9eab45b9e
CY Kalu <cy23@tutanota.com>
2021-08-26 11:59:13 +0000

    Return spinner while data is fetching

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -3,25 +3,34 @@ import PropTypes from 'prop-types';
 import ListGroup from 'react-bootstrap/ListGroup';
 import { moviedb } from '../api/movies';
 import ReviewItem from './ReviewItem';
+import Loading from './Loading';
 
 const MovieReviews = (props) => {
   const { movieID } = props;
   const [reviews, setReviews] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    moviedb.movieReviews(movieID).then((data) => setReviews(data.results));
+  useEffect(async () => {
+    await moviedb.movieReviews(movieID)
+      .then((data) => setReviews(data.results))
+      .catch((err) => err);
+    setLoading(false);
   }, []);
 
   return (
     <>
       <h5 className="sticky-top bg-dark py-2 text-decoration-underline border-secondary">Reviews</h5>
-      <ListGroup variamt="flush">
-        {
-          reviews.length > 0
-            ? reviews.map((review) => <ReviewItem review={review} key={review.id} />)
-            : <h2 className="my-auto opacity-25 fw-bold mb-0 lh-1">No reviews yet.</h2>
+      { loading
+        ? <Loading variant="sm" />
+        : (
+          <ListGroup variamt="flush">
+            {
+          reviews.length < 1
+            ? <h2 className="my-auto opacity-25 fw-bold mb-0 lh-1">No reviews yet.</h2>
+            : reviews.map((review) => <ReviewItem review={review} key={review.id} />)
         }
-      </ListGroup>
+          </ListGroup>
+        )}
     </>
   );
 };
