57342c9327c30de8b92c07c290a569983caa1e9c
CY Kalu <cy23@tutanota.com>
2021-08-27 01:30:20 +0000

    Add button css classes

--- src/components/ReviewItem.js
+++ src/components/ReviewItem.js
@@ -13,13 +13,13 @@ const ReviewItem = ({ review }) => {
   const toggleCollapse = () => setShowAll(!showAll);
 
   return (
-    <ListGroup.Item className="px-0 bg-transparent text-white border-0 border-bottom border-secondary" key={review.id}>
+    <ListGroup.Item className="px-0 bg-transparent text-white border-0 border-bottom border-secondary" key={review.id} data-testid="review-item">
       <h6 className="text-info mb-0 lh-1">{`${review.author}`}</h6>
       <div className="d-flex justify-content-between align-items-center mb-3">
         <small className="text-muted">{`Rating: ${review.author_details.rating}`}</small>
         {showAll && (
-        <Button variant="link" className="p-0 ms-4 lh-1" onClick={toggleCollapse}>
-          <small>Show less</small>
+        <Button variant="link" className="text-light lh-1 py-0 pb-1 text-decoration-none" onClick={toggleCollapse}>
+          <small>{showAll ? 'Show less' : 'Show more'}</small>
         </Button>
         )}
       </div>
@@ -27,17 +27,16 @@ const ReviewItem = ({ review }) => {
       {splittedContent.map((line, index) => (
         <p className="mb-2 opacity-75 review-text" key={Math.random()}>
           {line}
-          {index === splittedContent.length - 1 && content.length > sliceLength ? (
-            <>
-              ...
-              <br />
-              <Button variant="link" onClick={toggleCollapse} className="lh-1 border-0 p-0">
-                <small>{showAll ? 'Show less' : 'Show more'}</small>
-              </Button>
-            </>
-          ) : null}
+          { (!showAll && index === splittedContent.length - 1 && content.length > sliceLength) && <span className="text-muted fw-bold">...</span> }
         </p>
       ))}
+      <div className="d-flex mt-1 opacity-100">
+        <Button variant="info" className="lh-1 py-0 pb-1 rounded-0 text-decoration-none" onClick={toggleCollapse}>
+          <small className="fw-bold" style={{ fontSize: '13px' }}>
+            {showAll ? 'Show less' : 'Show more'}
+          </small>
+        </Button>
+      </div>
     </ListGroup.Item>
   );
 };
