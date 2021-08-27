e696d640ee760ad6551b2aef7728457c0418f39f
CY Kalu <cy23@tutanota.com>
2021-08-27 18:14:18 +0000

    Add list variants and test component

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -8,9 +8,10 @@ import Loading from './Loading';
 const MovieReviews = (props) => {
   const { movieID } = props;
   const [reviews, setReviews] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   useEffect(async () => {
+    setLoading(true);
     await moviedb.movieReviews(movieID)
       .then((data) => setReviews(data.results))
       .catch((err) => err);
@@ -21,14 +22,12 @@ const MovieReviews = (props) => {
     <>
       <h5 className="sticky-top bg-dark py-2 text-decoration-underline border-secondary">Reviews</h5>
       { loading
-        ? <Loading variant="sm" />
+        ? <Loading size="1rem" />
         : (
-          <ListGroup variamt="flush">
-            {
-          reviews.length < 1
-            ? <h2 className="my-auto opacity-25 fw-bold mb-0 lh-1">No reviews yet.</h2>
-            : reviews.map((review) => <ReviewItem review={review} key={review.id} />)
-        }
+          <ListGroup as="ul" variant="flush" data-testid="review-list">
+            { reviews.length < 1
+              ? <h2 className="my-auto opacity-25 fw-bold mb-0 lh-1">No reviews yet.</h2>
+              : reviews.map((review) => <ReviewItem review={review} key={review.id} />)}
           </ListGroup>
         )}
     </>
